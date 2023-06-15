8cbc22f836ff6e6673fc4645368ad67ba442812d
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-06-15 17:42:16 +0200

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - this program show how can u use print any message using puts
+ * Return: 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
