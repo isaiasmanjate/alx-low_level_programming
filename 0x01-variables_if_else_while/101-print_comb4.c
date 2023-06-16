ca86f1613964055ea9788d070ffeaea20d50bc1c
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-06-17 01:34:20 +0200

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - A program
+ * Return: 0
+ */
+int main(void)
+{
+	int d1, d2, d3;
+
+	for (d1 = 0; d1 <= 7; d1++)
+	{
+		for (d2 = 1 + d1; d2 <= 8; d2++)
+		{
+			for (d3 = d2 + 1; d3 <= 9; d3++)
+			{
+				putchar(d1 + '0');
+				putchar(d2 + '0');
+				putchar(d3 + '0');
+
+				if (d1 != 7 || d2 != 8 || d3 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
