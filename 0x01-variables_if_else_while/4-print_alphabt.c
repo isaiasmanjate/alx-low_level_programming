a235fb9d60b4716193a88d761666c89e34aad332
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-06-17 00:26:16 +0200

    task 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that print alphabet is lower case except q and e
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	{
+		if (character != 'e' && character != 'q')
+		{
+			putchar(character);
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
