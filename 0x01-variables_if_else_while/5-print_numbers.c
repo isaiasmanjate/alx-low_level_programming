1cd38e4fdefe2f3bf5dd04ab6395cb7277609f10
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-06-17 00:34:20 +0200

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that print all single digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf(n);
+	}
+	printf("\n");
+	return (0);
+}
