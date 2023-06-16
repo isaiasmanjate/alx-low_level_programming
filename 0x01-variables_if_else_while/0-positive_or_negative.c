bf4692966fd5563000dda3c13310ea207515f14c
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-06-16 23:06:17 +0200

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that print a random numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d %s\n", n, "is positive");
+		}
+	else
+	{
+		if (n < 0)
+		{
+			printf("%d %s\n", n, "is negative");
+		}
+		else
+		{
+			printf("%d %s\n", n, "is zero");
+		}
+	}
+	return (0);
+}
