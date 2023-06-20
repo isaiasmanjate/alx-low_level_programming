865b2b28f83886a33e2c4c4c2f6ec5af00f869e5
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-06-20 22:44:06 +0200

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+   main - A program that print _putchar
+Return: 0
+*/
+int main(void)
+{
+	const char msg [] ="_putchar\n";
+	int length = sizeof(msg) - 1;
+
+	for(int i = 0; i < length; i++)
+	{
+		write(1, $msg[i], 1);
+	}
+	return (0);
+}
+
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Im staring functions in C
