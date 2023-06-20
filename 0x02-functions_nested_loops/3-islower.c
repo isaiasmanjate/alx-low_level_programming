b134bc45dbe5e8e98fe07f87ee0c5aedb3d9d5e8
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-06-20 23:55:38 +0200

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower - A program thar verify alphabet
+* @c: is char to be checked
+* Return: 1 if is lower and then 1 if is not lower
+*/
+int _islower(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
