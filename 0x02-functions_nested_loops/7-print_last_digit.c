e27da67370ee0feb1e80a26025010ebccea806c9
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-06-21 00:53:51 +0200

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,21 +9,14 @@ int print_last_digit(int n)
 	int a;
 
 	if (n < 0)
-	{
 		n = -n;
-	}
-	else
-	{
-		a = n % 10;
+
+	a = n % 10;
+
 	if (a < 0)
-	{
 		a = -a;
-	}
-	else
-	{
-		_putchar(a + '0');
-	}
-	}
+
+	_putchar(a + '0');
+
 	return (a);
 }
-
